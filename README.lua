613334d9a92ca0be780405d532c97e8bb6099749
Mapachito <58920957+EsMapachito@users.noreply.github.com>
2020-08-17 21:59:29 +0200

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,6 +1,7 @@
 -- RECURSO CREADO PARA EL SERVIDOR RP DESPERADOSRP.COM --
 -- Implementado por Mapachito --
 1. /apoyar para apoyarse.
+/stop para detener la animación.
 SI UTILIZAS EL RECURSO, MENCIONAME Y CONTRIBUYE A QUE LA COMUNIDAD ESPAÑOLA DE REDM CREZCA ;)
 
 A disfrutarlo!!
